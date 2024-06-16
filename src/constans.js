92bd5f24df2c5619664957d404ebdd25087b0f29
Любовь Хуснуллина <lyuba140296@gmail.com>
2024-06-17 01:57:27 +0500

    add card

--- src/constans.js
+++ src/constans.js
@@ -1,43 +1,46 @@
+import cart1 from "./img/cart_img.png";
+import cart2 from "./img/cart_img2.png";
+
 export const CardsAll = [
   {
     id: 1,
     category:'Category | Marketing',
-    imgSrc: "images/cart_img.png",
+    imgSrc: cart1,
     altCard: "cardPicture",
     colorCard:'blue',
   },
   {
     id: 2,
     category:'Category',
-    imgSrc: "images/cart_img2.png",
+    imgSrc: cart2,
     altCard: "cardPicture",
     colorCard:'blue',
   },
   {
     id: 3,
     category:'Category | Development',
-    imgSrc: "images/cart_img.png",
+    imgSrc: cart1,
     altCard: "cardPicture",
     colorCard: 'darkblue',
   },
   {
     id: 4,
     category:'Category | Marketing',
-    imgSrc: "images/cart_img2.png",
+    imgSrc: cart2,
     altCard: "cardPicture",
     colorCard: 'darkblue',
   },
   {
     id: 5,
     category:'Category | Marketing',
-    imgSrc: "images/cart_img.png",
+    imgSrc: cart1,
     altCard: "cardPicture",
     colorCard:'blue',
   },
   {
     id: 6,
     category:'Category | Marketing',
-    imgSrc: "images/cart_img2.png",
+    imgSrc: cart2,
     altCard: "cardPicture",
     colorCard: 'darkblue',
   },
