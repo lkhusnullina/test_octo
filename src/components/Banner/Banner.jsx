8803a2df351b3abe73a1e353066c91d06d1c6277
Любовь Хуснуллина <lyuba140296@gmail.com>
2024-06-23 23:44:56 +0500

    Updates

--- src/components/Banner/Banner.jsx
+++
@@ -1,9 +0,0 @@
-import styles from "./Banner.module.css";
-
-export function Banner() {
-    return (
-        <div>
-            <h1 className={styles.header}>Solutions for people like you</h1>
-        </div>
-    )
-}
